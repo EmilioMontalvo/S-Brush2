c6d9675989ebf09e5b405f61af53c26c910c36c0
WELLINTON MARCELO PERALTA MORETA <wellinton.peralta@epn.edu.ec>
2022-02-10 22:49:43 -0500

    p

--- src/Interfaz/JFMenu.java
+++ src/Interfaz/JFMenu.java
@@ -2,6 +2,7 @@
 HOLA XDXD
 BJKBJKBKJBKJBKJBKJBK
 BJKNBJBBKJBJKBJK
+wellinton
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
